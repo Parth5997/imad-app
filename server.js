7260e7bf822b402fffbcfb3bae13d664e6394d74
Parth5997 <parth.parth.chauhan7@gmail.com>
2018-04-17 22:34:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/art1',function(req,res){
+    res.send("art1 requested and will be served here");
+})
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
