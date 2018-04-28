ac3c1883cb2697e0ab40a5dd9f75fcb6b86befc5
Parth5997 <parth.parth.chauhan7@gmail.com>
2018-04-28 11:56:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require(pg).Pool;
+
+var config = {
+    
+    user:"parthparthchauhan7" ,
+    database:"parthparthchauhan7" ,
+    host:"db.imad.hasura-app.io" ,
+    port: "5432" ,
+    password:process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -13,6 +26,24 @@ app.get('/art1',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'art1.html'));
     
 });
+
+
+var poll=new Pool(Config);
+app.get('/test-db',function(req,res)
+{
+    pool.query(SELECT * FROM test",function(err,result)
+    {
+        if(err)
+        {
+            res.status(500).send(err.toString())
+        }
+        
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    }
+}
 
 app.get('/parth',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'parth.html'));
